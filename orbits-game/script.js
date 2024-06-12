e8b2e7c2e20f9657989a7a92e55b5e836b3b9c49
Luke Longren <lcubelongren@gmail.com>
2024-06-11 22:58:33 -1000

    worked on visuals

--- orbits-game/script.js
+++ orbits-game/script.js
@@ -1,11 +1,11 @@
 
 let frame_rate = 60;
-let time_step = 10;
+let time_step = 20;
 
 let G = 6.67408e-11; // m3 kg-1 s-2
 let unit_modifier = 5e4;  // m px-1
 
-let player_acceleration = 2e-4 * unit_modifier;
+let player_acceleration = 2e-3 * unit_modifier / time_step;
 let player_rotation = 3e0;
 
 // Helper function to convert units while getting transform.
@@ -18,22 +18,29 @@ function m2pxTransform(body, unit_modifier) {
 	return transform_str;
 }
 
-let body_names = ['player', 'planet'];
+let body_names = ['player', 'planet', 'station'];
 let bodies = {}
 for (name of body_names) {
 	bodies[name] = { position: [0, 0], velocity: [0, 0], acceleration: [0, 0], mass: 1, rotation: null, size: null };
 }
 
-bodies['player']['position'] = [0, -(418e3+6371e3)];
-bodies['player']['velocity'] = [7.67e3, 0];
-bodies['player']['mass'] = 4.5e5;
+bodies['player']['position'] = [0, -(23222e3+6371e3)];
+bodies['player']['velocity'] = [3.68e3, 0];
+bodies['player']['mass'] = 675;
 bodies['player']['rotation'] = 90;
 bodies['player']['size'] = 25;
 
 bodies['planet']['mass'] = 6e24;
 bodies['planet']['size'] = 2*6371e3;
 
+bodies['station']['position'] = [0, ((422e3+413e3)/2+6371e3)];
+bodies['station']['velocity'] = [7.67e3, 0];
+bodies['station']['mass'] = 4.5e5;
+bodies['station']['rotation'] = 0;
+bodies['station']['size'] = 25;
+
 document.documentElement.style.setProperty('--player-size', Number.parseFloat(bodies['player']['size']) + 'px');
+
 document.getElementById('planet').style.height = Number.parseFloat(bodies['planet']['size'] / unit_modifier) + 'px';
 document.getElementById('planet').style.width = Number.parseFloat(bodies['planet']['size'] / unit_modifier) + 'px';
 
@@ -43,9 +50,7 @@ let background = document.getElementById('background');
 for (let i = 0; i < star_num; i++) {
 	let [star_x, star_y, star_r] = [Math.random() * 100, Math.random() * 100, Math.random() * 0];
 	let star_transform = `transform: translate(${star_x}vw, ${star_y}vh) rotate(${star_r}deg);`;
-	let star_duration = Math.random() * (3 - 2) + 2;
-	let star_animation = `animation: star ${star_duration}s linear infinite;`;
-	background.innerHTML += `<div class='star' style='${star_transform} ${star_animation}'></div>`;
+	background.innerHTML += `<div class='star' style='${star_transform}'></div>`;
 }
 
 const controller = {
@@ -89,6 +94,15 @@ let view_modifier = 1;
 
 const executeMoves = () => {
 	
+	let orbit_period;
+	// Rotate planet on its axis.
+	//orbit_period = (14 * 60) + 4.75;  // minutes
+	orbit_period = 24 * 60;  // minutes
+	bodies['planet']['rotation'] -= (360 / (60 * orbit_period)) * time_step;
+	// Rotate station along orbit.
+	orbit_period = 92.9 - 0.7;  // minutes
+	bodies['station']['rotation'] -= (360 / (60 * orbit_period)) * time_step;
+	
 	// Move the player with engines and gravity.
 	bodies['player']['acceleration'] = [0, 0];
 	Object.keys(controller).forEach(key => {
@@ -100,7 +114,9 @@ const executeMoves = () => {
 		}
 	})
 	bodies = Gravity(bodies, time_step, G);  // from ./motion.js
-	document.getElementById('player').style.transform = m2pxTransform(bodies['player'], unit_modifier);
+	for (target_name in bodies) {
+		document.getElementById(target_name).style.transform = m2pxTransform(bodies[target_name], unit_modifier);
+	}
 
 	// Show and hide the fire animations.
 	document.getElementById('player-fire-down').style.visibility = 'hidden';
@@ -125,10 +141,11 @@ const executeMoves = () => {
 	// Move the viewport in and out as the player moves.
 	let view_min = 2*6371e3;  // m
 	let view_max = 6*6371e3;  // m
-	if ((Math.abs(player_x) > view_max) || (Math.abs(player_y) > view_max)) {
+	let player2planet_distance = Math.abs(Math.sqrt(player_x**2 + player_y**2));
+	if (player2planet_distance > view_max) {
 	}
-	else if ((Math.abs(player_x) > view_min) || (Math.abs(player_y) > view_min)) {
-		view_modifier = Math.max(Math.abs(player_x), Math.abs(player_y)) / view_min;
+	else if (player2planet_distance > view_min) {
+		view_modifier = player2planet_distance / view_min;
 		view_modifier = 1 + Math.log(view_modifier**3)
 	}
 	else {
@@ -136,8 +153,8 @@ const executeMoves = () => {
 	}
 	unit_modifier = 5e4 * view_modifier;
 	document.documentElement.style.setProperty('--player-size', Number.parseFloat(bodies['player']['size'] / view_modifier) + 'px');
-	document.getElementById('planet').style.height = Number.parseFloat(bodies['planet']['size'] / (unit_modifier * view_modifier)) + 'px';
-	document.getElementById('planet').style.width = Number.parseFloat(bodies['planet']['size'] / (unit_modifier * view_modifier)) + 'px';
+	document.getElementById('planet').style.height = Number.parseFloat(bodies['planet']['size'] / unit_modifier) + 'px';
+	document.getElementById('planet').style.width = Number.parseFloat(bodies['planet']['size'] / unit_modifier) + 'px';
 	
 }
 setInterval(function(){ executeMoves() }, 1000*(1/frame_rate))
