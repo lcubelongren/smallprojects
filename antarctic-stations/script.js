29773682a92c3efdf82660aa3c391400b0feaa18
Luke Longren <lcubelongren@gmail.com>
2024-08-25 22:46:10 -0600

    restructured and added functionality

--- antarctic-stations/script.js
+++ antarctic-stations/script.js
@@ -56,57 +56,71 @@ async function fetchPage(url) {
 		return {};
 	}
     
-	try {
-		
-		const response = await fetch(url);
-		const page = await response.text();
-		let parser = new DOMParser();
-		let htmlDoc = parser.parseFromString(page, 'text/html');
-		
-		//stationsDoc = htmlDoc.getElementById('ann-stations')
-		stationsDoc = htmlDoc.getElementById('perm-stations')
-		for (report_item of stationsDoc.getElementsByClassName('report--item')) {
-			let item = {};
-			for (child of report_item.children) {
-				if (child.className == 'report--item__header') {
-					let header = child.getElementsByClassName('row')[1].innerText.trim().split('\n');
-					let NAME = header.at(0);
-					let TYPE = header.at(-1).trim();
-					item['NAME'] = NAME;
-					item['TYPE'] = TYPE;
-				}
+	const response = await fetch(url);
+	const page = await response.text();
+	let parser = new DOMParser();
+	let htmlDoc = parser.parseFromString(page, 'text/html');
+	
+	function LatLonStr2Float(LatLonStr) {
+		let L_str = LatLonStr.replace(/[^\d.]+/g, ' ').trim().split(' ');
+		let L = parseInt(L_str[0]);
+		if (L_str.length > 1) {
+			L = L + parseFloat(L_str[1]) / 60;
+			if (L_str.length > 2) {
+				L = L + parseFloat(L_str[2]) / (60 * 60);
 			}
-			for (child of report_item.getElementsByClassName('report--item--header--title__value fs14 fw-500')) {
-				let line = child.innerText.trim().split('\n');
-				if (line[0].slice(0, 10) == 'Site Name:') {
-					function LatLonStr2Float(LatLonStr) {
-						let L_str = LatLonStr.replace(/[^\d .]+/g, ' ').split(' ').filter(Number);
-						let L = parseInt(L_str[0]);
-						if (L_str.length > 1) {
-							L = L + parseFloat(L_str[1]) / 60;
-							if (L_str.length > 2) {
-								L = L + parseFloat(L_str[2]) / (60 * 60);
-							}
-						}
-						if (LatLonStr.includes('S') || LatLonStr.includes('W')) {
-							L = L * -1;
-						}
-						return L;
-					}
-					let LAT = LatLonStr2Float(line[1])
-					let LON = LatLonStr2Float(line[2])
-					item['LAT'] = LAT;
-					item['LON'] = LON;
-				}
-			}
-			items.push(item);
 		}
-		
-	return items;
-
-	} catch (error) {
-		console.error("Error:", error);
+		if (LatLonStr.includes('S') || LatLonStr.includes('W')) {
+			L = L * -1;
+		}
+		return L;
 	}
+	
+	//stationsDoc = htmlDoc.getElementById('ann-stations');
+	stationsDoc = htmlDoc.getElementById('perm-stations');
+	for (report_item of stationsDoc.getElementsByClassName('report--item')) {
+		let item = {};
+		for (child of report_item.children) {
+			if (child.className == 'report--item__header') {
+				let row = child.getElementsByClassName('row');
+				let header = row[1].innerText.trim().split('\n');
+				item['NAME'] = header.at(0);
+				item['TYPE'] = header.at(-1).trim();
+			}
+		}
+		for (child of report_item.getElementsByClassName('report--item--header--title__value fs14 fw-500')) {
+			let line = child.innerText.trim().split('\n');
+			if (line[0].slice(0, 10) == 'Site Name:') {
+				item['LAT'] = LatLonStr2Float(line[1]);
+				item['LON'] = LatLonStr2Float(line[2]);
+			}
+		}
+		items.push(item);
+	}
+	
+	arsDoc = htmlDoc.getElementById('perm-ars');
+	for (report_item of arsDoc.getElementsByClassName('report--item')) {
+		let item = {};
+		for (child of report_item.children) {
+			if (child.className == 'report--item__header') {
+				for (child of report_item.getElementsByClassName('report--item--header--title__value fs14 fw-500')) {
+					let row = child.innerHTML.split('</span>');
+					if (row[0].split('<strong>')[1] == 'Site Name</strong>: ') {
+						item['NAME'] = row[1].split('&nbsp;&nbsp;-')[0];
+						item['TYPE'] = 'Automatic Recording Station';
+						item['LAT'] = LatLonStr2Float(row[2].split('&nbsp;&nbsp;-')[0]);
+						item['LON'] = LatLonStr2Float(row[3].split('&nbsp;&nbsp;-')[0]);
+					}
+				}
+			}
+		}
+		items.push(item)
+	}
+
+	// https://stackoverflow.com/a/58437069
+	let unique_items = items.filter((v, i, a) => a.findIndex(v2 => (JSON.stringify(v) === JSON.stringify(v2))) === i)
+	return unique_items;
+
 }
 
 // --------------------------------
@@ -167,11 +181,15 @@ async function main() {
 						context.fillStyle = countries[data['country']]['color'];
 						context.strokeStyle = 'white';
 						if (item['TYPE'] == 'Station') {
-							geoStation.radius(0.5);
+							geoStation.radius(0.6);
 							context.globalCompositeOperation = 'source-over';
 						}
-						else {
-							geoStation.radius(0.3);
+						else if (item['TYPE'] == 'Automatic Recording Station') {
+							geoStation.radius(0.2);
+							context.globalCompositeOperation = 'destination-over';
+						}
+						else {  // misc camps
+							geoStation.radius(0.4);
 							context.globalCompositeOperation = 'destination-over';
 						}
 						geoStation.center([item['LON'], item['LAT']]);
@@ -204,23 +222,27 @@ async function main() {
 	
 	}
 	
-	drawMap();
+	drawMap(exclude_types);
 
 	var tooltip = document.getElementById('tooltip');
 	d3.select('canvas').on('mousemove', (event) => {
 		const { width, height } = canvas.getBoundingClientRect();
 		let mouse = projection.invert([d3.pointer(event)[0] * canvas.width / width, 
 		                               d3.pointer(event)[1] * canvas.height / height]);
+		let tooltipHTML = '';
 		let tooltipOn = false;
 		for (data of country_data) {
 			for (item of data['items']) {
 				if (exclude_types.includes(item['TYPE']) == false) {
-					if (d3.geoDistance(mouse, [item['LON'], item['LAT']]) < 0.01) {
+					let pointer_distance = 0.007;
+					if (item['TYPE'] == 'Station') { pointer_distance = 0.01; }
+					else if (item['TYPE'] == 'Automatic Recording Station') { pointer_distance = 0.004; }
+					if (d3.geoDistance(mouse, [item['LON'], item['LAT']]) < pointer_distance) {
 						if (countries[data['country']]['display'] == true) {
-							tooltip.style.display = 'block';
-							tooltip.style.left = (event.pageX + 10) + 'px';
-							tooltip.style.top = (event.pageY + 10) + 'px';
-							tooltip.innerHTML = item['NAME'] + '<br><i>' + item['TYPE'] + '</i><br><b>' + data['country'] + '</b>';
+							if (tooltipOn == true) {
+								tooltipHTML += '<hr>';
+							}
+							tooltipHTML += item['NAME'] + '<br><i>' + item['TYPE'] + '</i><br><b>' + data['country'] + '</b><br>';
 							tooltipOn = true;
 						}
 					}
@@ -228,7 +250,22 @@ async function main() {
 			}
 		}
 		if (tooltipOn == false) {
+			tooltip.innerHTML = '';
 			tooltip.style.display = 'none';
+		}
+		else {
+			tooltip.innerHTML = tooltipHTML;
+			tooltip.style.display = 'block';
+			let xoffset = event.pageX;
+			let yoffset = event.pageY;
+			if (mouse[0] > 0) {  // right half of page
+				xoffset -= tooltip.offsetWidth;
+			}
+			if (Math.abs(mouse[0]) > 90) {  // bottom half of page
+				yoffset -= tooltip.offsetHeight;
+			}
+			tooltip.style.left = xoffset + 'px';
+			tooltip.style.top = yoffset + 'px';
 		}
 	})
 	
@@ -260,7 +297,7 @@ async function main() {
 		flag.addEventListener('click', function (event) {
 			let country_name = event.target.id.split('-').slice(1)[0];
 			toggleCountry(country_name);
-			drawMap();
+			drawMap(exclude_types);
 		})
 	}
 	
@@ -274,11 +311,15 @@ async function main() {
 	toolbar.innerHTML += '<span>toggle all:</span>'
 	toolbar.innerHTML += '<span class="toggle">' +
 	                     '<input id="toggle-stations" type="checkbox" checked>' +
-						 '<label for="toggle-stations">stations</label>' +
+						 '<label for="toggle-stations">main<br>stations</label>' +
 						 '</span>';
 	toolbar.innerHTML += '<span class="toggle">' +
 	                     '<input id="toggle-camps" type="checkbox" checked>' +
-						 '<label for="toggle-camps">camps</label>' +
+						 '<label for="toggle-camps">field<br>camps</label>' +
+						 '</span>';
+	toolbar.innerHTML += '<span class="toggle">' +
+	                     '<input id="toggle-ars" type="checkbox" checked>' +
+						 '<label for="toggle-ars">automated<br>stations</label>' +
 						 '</span>';
 	toolbar.innerHTML += '<span class="toggle">' +
 	                     '<input id="toggle-countries" type="checkbox" checked>' +
@@ -304,6 +345,15 @@ async function main() {
 		}
 		drawMap(exclude_types)
 	})
+	document.getElementById('toggle-ars').addEventListener('change', function(event) {
+		if (event.target.checked) {
+			exclude_types = exclude_types.filter(e => e !== 'Automatic Recording Station');
+		}
+		else {
+			exclude_types.push('Automatic Recording Station');
+		}
+		drawMap(exclude_types)
+	})
 	document.getElementById('toggle-countries').addEventListener('change', function(event) {
 		for (country_name of Object.keys(countries)) {
 			if (event.target.checked) {
@@ -320,6 +370,17 @@ async function main() {
 		drawMap(exclude_types);
 	})
 	
+	let info = document.getElementById('info');
+	info.addEventListener('click', function (event) {
+		info.style.transition = 'all 0.3s ease-out';
+		if (info.className == '') {
+			info.className = 'active';
+		}
+		else {
+			info.className = '';
+		}
+	})
+	
 	function positionFlags() {
 		if (canvas.getBoundingClientRect().left > 0) {
 			flags.style.right = canvas.getBoundingClientRect().left + 'px';
@@ -330,14 +391,28 @@ async function main() {
 	}
 	function positionToolbar() {
 		toolbar.style.bottom = canvas.getBoundingClientRect().top + 'px';
-		toolbar.style.visibility = 'visible';
+	}
+	function positionInfo() {
+		info.style.transition = '';
+		if (canvas.getBoundingClientRect().left > 0) {
+			info.style.left = canvas.getBoundingClientRect().left + 'px';
+		}
+		else {
+			info.style.left = 0;
+		}
+		info.style.top = canvas.getBoundingClientRect().top + 'px';
 	}
 	positionFlags();
 	positionToolbar();
+	positionInfo();
 	window.addEventListener('resize', function (event) {
 		positionFlags();
 		positionToolbar();
+		positionInfo();
 	})
+	
+	toolbar.style.visibility = 'visible';
+	info.style.visibility = 'visible';
 
 	document.getElementById('loading').remove();
 }
