e3b8c15c4457495462117508aa51fdc37b4ea949
Luke Longren <lcubelongren@gmail.com>
2024-10-17 20:58:53 -0600

    added functionality

--- airline-destinations/script.js
+++ airline-destinations/script.js
@@ -1,4 +1,6 @@
 
+var chosen_airline = 'UA';
+
 async function main() {
 	
 	async function loadData() {
@@ -32,77 +34,86 @@ async function main() {
 	
 	let [airline_data, land_data, lake_data] = await loadData();
 	
-	canvas.width = window.innerWidth;
-	canvas.height = window.innerHeight;
-
-	let context = d3.select('#canvas')
-	.node()
-	.getContext('2d');
-
-	let projection = d3.geoAzimuthalEquidistant()
-	.rotate([90, -45])
-	.scale(canvas.height * 0.66)
-	.translate([canvas.width / 2, canvas.height / 2]);
-
-	let graticule = d3.geoGraticule();
-	let geoGenerator = d3.geoPath(projection, context);
-	let geoAirport = d3.geoCircle();
-	
-	let colors = d3.scaleSequential(d3.interpolateViridis);
-	
 	let all_years = Object.keys(airline_data);
 	
-	function drawMap(years) {
+	let colors = d3.scaleSequential(d3.interpolateWarm);
+	
+	function initMap(years, chosen_airline) {
 		
-		context.clearRect(0, 0, canvas.width, canvas.height);
-		
-		context.beginPath();
-		geoGenerator({type: 'FeatureCollection', features: land_data.features})
-		context.fillStyle = 'white';
-		context.fill();
-		
-		context.beginPath();
-		geoGenerator({type: 'FeatureCollection', features: lake_data.features})
-		context.fillStyle = 'black';
-		context.fill();
-		
-		context.beginPath();
-		geoGenerator(graticule());
-		context.strokeStyle = '#a9a9a9';
-		context.lineWidth = 2;
-		context.stroke();
+		canvas.width = window.innerWidth;
+		canvas.height = window.innerHeight;
 
-		geoAirport.radius(0.5);
-		context.strokeStyle = 'black';
-		context.lineWidth = 1.0;
-		for (airline of Object.keys(airline_data[year])) {
-			let airports_plotted = [];
-			let include_airlines = ['UA'];
-			for (year of years) {
-				if (include_airlines.includes(airline) & Object.keys(airline_data[year]).includes(airline)) {
-					let color_idx = (year - Math.min(...all_years)) / (all_years.length - 1);
-					context.beginPath();
-					context.fillStyle = colors(color_idx);
-					for (airport of Object.keys(airline_data[year][airline])) {
-						if (airports_plotted.includes(airport) == false) {
-							airports_plotted.push(airport);
-							let lat = airline_data[year][airline][airport]['lat'];
-							let lon = airline_data[year][airline][airport]['lon'];
-							geoAirport.center([lon, lat]);
-							geoGenerator(geoAirport());
+		let context = d3.select('#canvas')
+		.node()
+		.getContext('2d');
+
+		let projection = d3.geoOrthographic()
+		.rotate([90, -45])
+		.scale(canvas.height * 0.66)
+		.translate([canvas.width / 2, canvas.height / 2]);
+
+		let graticule = d3.geoGraticule();
+		let geoGenerator = d3.geoPath(projection, context);
+		let geoAirport = d3.geoCircle();
+		
+		function drawMap(years, chosen_airline) {
+			
+			context.clearRect(0, 0, canvas.width, canvas.height);
+			
+			context.beginPath();
+			geoGenerator({type: 'FeatureCollection', features: land_data.features})
+			context.fillStyle = 'white';
+			context.fill();
+			
+			context.beginPath();
+			geoGenerator({type: 'FeatureCollection', features: lake_data.features})
+			context.fillStyle = 'black';
+			context.fill();
+			
+			context.beginPath();
+			geoGenerator(graticule());
+			context.strokeStyle = '#a9a9a9';
+			context.lineWidth = 1.5;
+			context.stroke();
+
+			geoAirport.radius(0.5);
+			context.strokeStyle = 'black';
+			context.lineWidth = 1.0;
+			for (airline of Object.keys(airline_data[year])) {
+				let airports_plotted = [];
+				for (year of years) {
+					if ((airline == chosen_airline) & Object.keys(airline_data[year]).includes(airline)) {
+						let color_idx = (year - Math.min(...all_years)) / (all_years.length - 1);
+						context.beginPath();
+						context.fillStyle = colors(color_idx);
+						for (airport of Object.keys(airline_data[year][airline])) {
+							if (airports_plotted.includes(airport) == false) {
+								airports_plotted.push(airport);
+								let lat = airline_data[year][airline][airport]['lat'];
+								let lon = airline_data[year][airline][airport]['lon'];
+								geoAirport.center([lon, lat]);
+								geoGenerator(geoAirport());
+							}
 						}
+						context.fill();
+						context.stroke();
 					}
-					context.fill();
-					context.stroke();
+				}
+				if (airline == chosen_airline) {
+					document.getElementById('airlineInfo').innerText = 'Airline: ' + chosen_airline + '\n' +
+																	   'Destinations: ' + airports_plotted.length;
 				}
 			}
-			if (include_airlines.includes(airline)) {
-				document.getElementById('airlineInfo').innerText = 'Airline: ' + include_airlines[0] + '\n' +
-				                                                   'Destinations: ' + airports_plotted.length;
-			}
+
 		}
+		
+		drawMap(years, chosen_airline);
+		
+		return [drawMap, projection];
 	
 	}
+	
+	let [drawMap, projection] = initMap(all_years, chosen_airline);
 	
 	// https://medium.com/@predragdavidovic10/native-dual-range-slider-html-css-javascript-91e778134816
 	function fillSlider(from, to, sliderColor) {
@@ -134,28 +145,142 @@ async function main() {
 		document.getElementById('toYear').innerText = years.slice(-1);
 		document.getElementById('fromYear').style.color = colors(fromPosition / rangeDistance);
 		document.getElementById('toYear').style.color = colors(toPosition / rangeDistance);
-		drawMap(years);
+		drawMap(years, chosen_airline);
+		return years;
 	}
 	let fromInput = document.getElementById('yearRangeLow');
 	let toInput = document.getElementById('yearRangeHigh');
-	fromInput.min = Math.min(...all_years)
-	fromInput.max = Math.max(...all_years)
-	fromInput.value = Math.min(...all_years)
-	toInput.min = Math.min(...all_years)
-	toInput.max = Math.max(...all_years)
-	toInput.value = Math.max(...all_years)
-	adjustSlider(fromInput, toInput)
+	fromInput.min = Math.min(...all_years);
+	fromInput.max = Math.max(...all_years);
+	fromInput.value = Math.min(...all_years);
+	toInput.min = Math.min(...all_years);
+	toInput.max = Math.max(...all_years);
+	toInput.value = Math.max(...all_years);
+	var years = adjustSlider(fromInput, toInput);
 	document.getElementById('yearRangeHigh').addEventListener('input', function (event) {
-		adjustSlider(fromInput, toInput);
+		years = adjustSlider(fromInput, toInput);
 	})
 	document.getElementById('yearRangeLow').addEventListener('input', function (event) {
-		adjustSlider(fromInput, toInput);
+		years = adjustSlider(fromInput, toInput);
+	})
+	
+	var tooltip = document.getElementById('tooltip');
+	d3.select('canvas').on('mousemove', (event) => {
+		const { width, height } = canvas.getBoundingClientRect();
+		let mouse = projection.invert([d3.pointer(event)[0] * canvas.width / width, 
+		                               d3.pointer(event)[1] * canvas.height / height]);
+		let tooltipAirports = [];
+		let tooltipHTML = '';
+		let tooltipOn = false;
+		for (year of years) {
+			for (airline of Object.keys(airline_data[year])) {
+				if ((airline == chosen_airline) & Object.keys(airline_data[year]).includes(airline)) {
+					for (airport of Object.keys(airline_data[year][airline])) {
+						let pointer_distance = 0.0075;
+						let lat = airline_data[year][airline][airport]['lat'];
+						let lon = airline_data[year][airline][airport]['lon'];
+						if (d3.geoDistance(mouse, [lon, lat]) < pointer_distance) {
+							if (tooltipAirports.includes(airport) == false) {
+								if (tooltipOn == true) {
+									tooltipHTML += '<hr>';
+								}
+								tooltipHTML += '<b>' + airport + '</b>';
+								tooltipAirports.push(airport);
+								tooltipOn = true;
+							}
+						}
+						
+					}
+				}
+			}
+		}
+		if (tooltipOn == false) {
+			let tooltipAirports = [];
+			tooltip.innerHTML = '';
+			tooltip.style.display = 'none';
+		}
+		else {
+			tooltip.innerHTML = tooltipHTML;
+			tooltip.style.display = 'block';
+			let xoffset = event.pageX;
+			let yoffset = event.pageY;
+			xoffset -= tooltip.offsetWidth;
+			yoffset -= tooltip.offsetHeight;
+			tooltip.style.left = xoffset + 'px';
+			tooltip.style.top = yoffset + 'px';
+		}
+	})
+	
+	let info = document.getElementById('info');
+	info.addEventListener('click', function (event) {
+		info.style.transition = 'all 0.3s ease-out';
+		if (info.className == '') {
+			info.className = 'active';
+		}
+		else {
+			info.className = '';
+		}
+	})
+	info.style.transition = '';
+	info.style.visibility = 'visible';
+	let info_text = document.getElementById('info-text');
+	let possible_airlines = ['UA', 'DL', 'AA', 'AS'];
+	info_text.innerHTML = '<br>';
+	for (airline of possible_airlines) {
+		info_text.innerHTML += '<button id="button-' + airline + '" type="button">' + airline + '</button><br>';
+	}
+	for (airline of possible_airlines) {
+		document.getElementById('button-' + airline).addEventListener('click', function (event) {
+			chosen_airline = event.target.id.split('-')[1];
+			drawMap(years, chosen_airline);
+		})
+	}
+	
+	let rotationMagnitude = 7.5;
+	let scaleMagnitude = 50;
+	document.getElementById('controls-up').addEventListener('click', function (event) {
+		let rotation = projection.rotate().map(function (num, idx) {
+			return num + [0, -rotationMagnitude, 0][idx];
+		})
+		projection.rotate(rotation);
+		drawMap(years, chosen_airline);
+	})
+	document.getElementById('controls-down').addEventListener('click', function (event) {
+		let rotation = projection.rotate().map(function (num, idx) {
+			return num + [0, +rotationMagnitude, 0][idx];
+		})
+		projection.rotate(rotation);
+		drawMap(years, chosen_airline);
+	})
+	document.getElementById('controls-left').addEventListener('click', function (event) {
+		let rotation = projection.rotate().map(function (num, idx) {
+			return num + [+rotationMagnitude, 0, 0][idx];
+		})
+		projection.rotate(rotation);
+		drawMap(years, chosen_airline);
+	})
+	document.getElementById('controls-right').addEventListener('click', function (event) {
+		let rotation = projection.rotate().map(function (num, idx) {
+			return num + [-rotationMagnitude, 0, 0][idx];
+		})
+		projection.rotate(rotation);
+		drawMap(years, chosen_airline);
+	})
+	document.getElementById('controls-in').addEventListener('click', function (event) {
+		let scale = projection.scale() + scaleMagnitude;
+		projection.scale(scale);
+		drawMap(years, chosen_airline);
+	})
+	document.getElementById('controls-out').addEventListener('click', function (event) {
+		let scale = projection.scale() - scaleMagnitude;
+		projection.scale(scale);
+		drawMap(years, chosen_airline);
+	})
+	
+	window.addEventListener('resize', function (event) {
+		drawMap(years, chosen_airline);
 	})
 
 }
 
 main();
-
-window.addEventListener('resize', function (event) {
-	main();
-})
