13ffbf0997c4a52e5427f32447bd9db419310b57
Luke Longren <lcubelongren@gmail.com>
2025-02-23 02:12:44 -0700

    worked on adding data

--- airplane-manufacturing/preprocessing.py
+++ airplane-manufacturing/preprocessing.py
@@ -1,61 +1,92 @@
 
+import os, xlrd
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
 
+directory = pd.read_csv('data/Boeing_10-K/_directory.txt', index_col=0)
+
 count = {}
-years = np.arange(2003, 2025)
+years = np.arange(1970, 2025)
 for year in years:
-    print(year)
     count[year] = {}
-    data = pd.read_excel('data/10-K_{}_Boeing.xls'.format(year), sheet_name=None)
-    for sheet_name in data.keys():
-        if ('Cumulative deliveries' in data[sheet_name].values) or ('Cumulative Deliveries' in data[sheet_name].values):
-            break  # >= 2006
-        elif ('Model' in data[sheet_name].values) and (not 'Total' in data[sheet_name].values):
-            break  # >= 2002
-    if year >= 2006:
-        skiprows = np.where(data[sheet_name][data[sheet_name].columns[1]].values == str(year))[0][0] - 1
-        data_sheet = pd.read_excel('data/10-K_{}_Boeing.xls'.format(year), sheet_name=sheet_name, skiprows=skiprows, header=1)
-        cumulative = data_sheet.loc[1][data_sheet.loc[1].apply(lambda x: isinstance(x, float) and not pd.isna(x))]
-        cumulative.rename(index={'737 NG': '737'}, inplace=True)
-        for plane in cumulative.index:
-            count[year][str(plane)] = int(cumulative[plane])
-    elif year >= 2002:
-        planes = data[sheet_name][data[sheet_name].columns[1]].dropna()
-        planes.drop(planes.loc[planes == 'Model'].index, inplace=True)
-        planes.drop(planes.loc[planes == '-'].index, inplace=True)
-        cumulative = data[sheet_name][data[sheet_name].columns[3]]
-        for row_idx in planes.index:
-            count[year][str(planes[row_idx])] = int(cumulative[row_idx])
-    else:
-        # wrong sheet
-        skiprows = data[sheet_name][data[sheet_name][data[sheet_name].columns[3]] == year].index[0]
-        data_sheet = pd.read_excel('data/10-K_{}_Boeing.xls'.format(year), sheet_name=sheet_name, skiprows=skiprows, header=1)
-        planes = data_sheet[data[sheet_name].columns[1]].dropna()
-        planes.drop(planes.loc[planes == '='].index, inplace=True)
-        planes.drop(planes.loc[planes == '-'].index, inplace=True)
-        cumulative = data_sheet[data_sheet.columns[3]]
-        for row_idx in planes.index:
-            if planes[row_idx] in count[year]:
-                count[year][str(planes[row_idx])] += int(str(cumulative[row_idx]).split('(')[0])
-            else:
-                count[year][str(planes[row_idx])] = int(str(cumulative[row_idx]).split('(')[0])
-    print(count[year])
+    if (1993 <= year <= 2024):
+        sheet_name = directory.at[year, 'sheet_name']
+        skiprows = directory.at[year, 'skiprows']
+        workbook = xlrd.open_workbook('data/Boeing_10-K/10-K_{}_Boeing.xls'.format(year), logfile=open(os.devnull, 'w'))
+        data_sheet = pd.read_excel(workbook, sheet_name=sheet_name, skiprows=skiprows-2, header=1)
+        if year >= 2006:
+            data_sheet.drop([c for c in data_sheet.columns if 
+                             (str(c).split(':')[0] == 'Unnamed') or (c == 'Total') or (str(c)[0] == '*') or (str(c)[0] == '†')], 
+                             axis='columns', inplace=True)
+            planes = data_sheet.columns
+            for plane in planes:
+                num = data_sheet[plane][2]
+                if pd.isnull(num):
+                    count[year][str(plane)] = 0
+                else:
+                    count[year][str(plane)] = int(num)
+        else:
+            planes = data_sheet[data_sheet.columns[1]].dropna()
+            planes.drop(planes.loc[planes == '-'].index, inplace=True)
+            planes.drop(planes.loc[planes == '='].index, inplace=True)
+            planes.drop(planes.loc[planes == 'Total'].index, inplace=True)
+            for idx in planes.index:
+                plane = str(planes[idx]).split('*')[0]
+                num = float(str(data_sheet[year][idx]).split('(')[0])
+                if pd.isnull(num):
+                    count[year][str(plane)] = 0
+                else:
+                    count[year][str(plane)] = int(num)
+    elif (1970 <= year <= 1992):
+        data_sheet = pd.read_excel('data/Boeing_Annual-Reports.xlsx', header=0, index_col=0)
+        row = data_sheet.loc[year].dropna().drop('Total')
+        planes = list(row.index)
+        for plane in planes:
+            num = row[row.index == plane]
+            count[year][str(plane)] = int(num)
+    #print(year, count[year])
+
+plane_combos = {}
+target_planes = ['707', '717', '720', '727', '737', '747', '757', '767', '777', '787']
+for target_plane in target_planes:
+    plane_combos[target_plane] = {}
+    for year in years:
+        plane_combos[target_plane][year] = {}
+        for plane in count[year].keys():
+            if (plane.split(' ')[0] == target_plane) or (plane.split('-')[0] == target_plane) or (plane.split('B')[0] == target_plane):
+                plane_combos[target_plane][year][plane] = count[year][plane]
+for year in years:
+    for target_plane in plane_combos.keys():
+        target_plane_num = 0
+        sub_plane_list = []
+        if year in plane_combos[target_plane].keys():
+            for sub_plane in plane_combos[target_plane][year].keys():
+                sub_num = plane_combos[target_plane][year][sub_plane]
+                sub_plane_list.append(sub_plane)
+                target_plane_num += sub_num
+            for sub_plane in sub_plane_list:
+                del count[year][sub_plane]
+        if target_plane_num > 0:
+            count[year][str(target_plane)] = target_plane_num
+    #print(year, count[year])
 
 all_planes = []
 for year in years:
     for plane in count[year].keys():
+        if plane.split('-')[0] == 'MD':
+            continue
         if not (plane in all_planes):
             all_planes.append(plane)
 
 deliveries_unpacked = {plane: [count[year][plane] if plane in count[year].keys() else np.nan for year in years] for plane in all_planes}
 
-for plane in deliveries_unpacked.keys():
+plt.figure(figsize=(8, 6), dpi=300)
+for plane in np.sort(list(deliveries_unpacked.keys())):
     plt.plot(years, deliveries_unpacked[plane], label=plane)
-plt.xticks(years[::-2])
-plt.ylabel('Cumulative Deliveries')
+plt.xticks(years[::-10])
+plt.ylabel('Deliveries')
 plt.xlabel('Year')
 plt.legend()
 plt.savefig('plot.png')
