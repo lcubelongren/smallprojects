6e3ab5f56cbe3c9654edb5228a90516a99bf3105
Luke Longren <lcubelongren@gmail.com>
2025-02-23 21:55:14 -0700

    reworked the data sources, should be final now

--- airplane-manufacturing/preprocessing.py
+++ airplane-manufacturing/preprocessing.py
@@ -8,143 +8,126 @@ import matplotlib.pyplot as plt
 # Create a dictionary of plane delivery count by year.
 
 def Boeing():
+    print('Preprocessing for Boeing')
+    years = np.arange(1958, 2025)
     count = {}
-    years = np.arange(1964, 2025)
+    # 1958 to Present
+    fname = 'data/Boeing_OrdersandDeliveries.csv'
+    data = pd.read_csv(fname, header=0)
     for year in years:
         count[year] = {}
-        if (1993 <= year <= years[-1]):
-            directory = pd.read_csv('data/Boeing_10-K/_directory.txt', index_col=0)
-            sheet_name = directory.at[year, 'sheet_name']
-            skiprows = directory.at[year, 'skiprows']
-            workbook = xlrd.open_workbook('data/Boeing_10-K/10-K_{}_Boeing.xls'.format(year), logfile=open(os.devnull, 'w'))
-            data_sheet = pd.read_excel(workbook, sheet_name=sheet_name, skiprows=skiprows-2, header=1)
-            if year >= 2006:
-                data_sheet.drop([c for c in data_sheet.columns if 
-                                 (str(c).split(':')[0] == 'Unnamed') or (c == 'Total') or (str(c)[0] == '*') or (str(c)[0] == '†')], 
-                                 axis='columns', inplace=True)
-                planes = data_sheet.columns
-                for plane in planes:
-                    num = data_sheet[plane][2]
-                    if pd.isnull(num):
-                        count[year][str(plane)] = 0
-                    else:
-                        count[year][str(plane)] = int(num)
-            else:
-                planes = data_sheet[data_sheet.columns[1]].dropna()
-                planes.drop(planes.loc[planes == '-'].index, inplace=True)
-                planes.drop(planes.loc[planes == '='].index, inplace=True)
-                planes.drop(planes.loc[planes == 'Total'].index, inplace=True)
-                for idx in planes.index:
-                    plane = str(planes[idx]).split('*')[0]
-                    num = float(str(data_sheet[year][idx]).split('(')[0])
-                    if pd.isnull(num):
-                        count[year][str(plane)] = 0
-                    else:
-                        count[year][str(plane)] = int(num)
-        elif (years[0] <= year <= 1992):
-            data_sheet = pd.read_excel('data/Boeing_Annual-Reports.xlsx', index_col=0, header=0)
-            row = data_sheet.loc[year].dropna().drop('Total')
-            planes = list(row.index)
-            for plane in planes:
-                num = row[row.index == plane]
-                count[year][str(plane)] = int(num)
+        planes_listed = data['Model Series'][data['Delivery Year'] == str(year)]
+        planes = np.unique(planes_listed)
+        for plane in planes:
+            num_idxs = data['Delivery Total'].where(np.logical_and(data['Delivery Year'] == str(year), data['Model Series'] == plane))
+            num_idxs = num_idxs.dropna().index
+            num = np.sum(data['Delivery Total'][num_idxs].values.astype(int))
+            count[year][plane] = int(num)
         #print(year, count[year])
     target_planes = ['707', '717', '727', '737', '747', '757', '767', '777', '787']
-    count, all_planes = combinePlanes(count, years, target_planes)
-    plotPlanes(count, years, all_planes, name='Boeing')
+    count = combinePlanes(count, years, target_planes, name='Boeing')
+    plotPlanes(count, years, target_planes, name='Boeing')
     return count
 
 def Airbus():
+    print('Preprocessing for Airbus')
+    years = np.arange(1974, 2025)
     count = {}
-    years = np.arange(2006, 2024)
-    data_sheet = pd.read_excel('data/Airbus_OaD_2024_10.xlsx', sheet_name='Historical deliveries', skiprows=1, header=None)
-    data_sheet.dropna(axis=0, how='all', inplace=True)
-    data_sheet.dropna(axis=1, how='all', inplace=True)
-    for row_idx in data_sheet.index[::2]:
-        data_year = data_sheet.loc[row_idx:row_idx+1]
+    # 2010 to Present
+    years1 = np.arange(2010, 2025)
+    fname1 = 'data/Airbus_OaD-2025-01-fl9786.xlsx'
+    data1 = pd.read_excel(fname1, sheet_name='Historical deliveries', skiprows=0, header=None)
+    data1.dropna(axis=0, how='all', inplace=True)
+    data1.dropna(axis=1, how='all', inplace=True)
+    for row_idx in data1.index[::2]:
+        data_year = data1.loc[row_idx:row_idx+1]
         year = int(data_year.at[row_idx, 0].split(' ')[0])
+        if year in years1:
+            count[year] = {}
+            for col in range(1, 7):
+                plane = data_year.at[row_idx, col]
+                if not pd.isnull(plane):
+                    num = data_year.at[row_idx+1, col]
+                    if pd.isnull(num):
+                        count[year][str(plane)] = np.nan
+                    else:
+                        count[year][str(plane)] = int(num)
+            #print(year, count[year])
+    # 1974 to 2009
+    years2 = np.arange(1974, 2010)
+    fname2 = 'data/Airbus_Summary_Historial_Orders_Deliveries_1974-2009.xls'
+    data2 = pd.read_excel(fname2, sheet_name='HIST&orddel', usecols='O:Y', index_col=0, skiprows=3)
+    data2.columns = data2.loc['Year']
+    for year in years2:
         count[year] = {}
-        for col in range(1, 7):
-            plane = data_year.at[row_idx, col]
-            if not pd.isnull(plane):
-                num = data_year.at[row_idx+1, col]
-                if pd.isnull(num):
-                    count[year][str(plane)] = 0
-                else:
-                    count[year][str(plane)] = int(num)
+        for plane in data2.columns:
+            num = data2.at[year, plane]
+            if pd.isnull(num):
+                count[year][str(plane)] = np.nan
+            else:
+                count[year][str(plane)] = int(num)
         #print(year, count[year])
-    target_planes = ['A220', 'A310/A300-600', 'A320Family', 'A330', 'A340', 'A350', 'A380']
-    count, all_planes = combinePlanes(count, years, target_planes)
-    plotPlanes(count, years, all_planes, name='Airbus')
+    target_planes = ['A220', 'A300', 'A310', 'A320', 'A330', 'A340', 'A350', 'A380']
+    count = combinePlanes(count, years, target_planes, name='Airbus')
+    plotPlanes(count, years, target_planes, name='Airbus')
     return count
 
 # Helper functions.
 
-def combinePlanes(count, years, target_planes):
-    # Combine plane subtypes.
-    plane_combos = {}
-    for target_plane in target_planes:
-        plane_combos[target_plane] = {}
-        for year in years:
-            plane_combos[target_plane][year] = {}
-            for plane in count[year].keys():
-                characters = [' ', '-', '/', 'B']
-                for c in characters:
-                    if (plane.split(c)[0] == target_plane):
-                        plane_combos[target_plane][year][plane] = count[year][plane]
+def combinePlanes(count, years, target_planes, name):
+    new_count = {}
     for year in years:
-        for target_plane in plane_combos.keys():
-            target_plane_num = 0
-            sub_plane_list = []
-            if year in plane_combos[target_plane].keys():
-                for sub_plane in plane_combos[target_plane][year].keys():
-                    sub_num = plane_combos[target_plane][year][sub_plane]
-                    sub_plane_list.append(sub_plane)
-                    target_plane_num += sub_num
-                for sub_plane in sub_plane_list:
-                    del count[year][sub_plane]
-            if target_plane_num > 0:
-                count[year][str(target_plane)] = target_plane_num
-        #print(year, count[year])
-
-    # Make a list of all planes.
-    all_planes = []
-    for year in years:
+        new_count[year] = {}
+        target_planes_num = np.zeros(len(target_planes))
         for plane in count[year].keys():
-            if not plane in target_planes:
-                continue
-            if not (plane in all_planes):
-                all_planes.append(plane)
-    
-    return count, all_planes
+            for i,target_plane in enumerate(target_planes):
+                if name == 'Boeing':
+                    if plane[:3] == target_plane[:3]:
+                        target_planes_num[i] += count[year][plane]
+                if name == 'Airbus':
+                    if ((target_plane == 'A320') and (plane in ['A320Family', 'A318', 'A319', 'A320', 'A321'])) or (plane == target_plane):
+                        if not np.isnan(count[year][plane]):
+                            target_planes_num[i] += count[year][plane]         
+        for i,target_plane in enumerate(target_planes):
+            if (target_planes_num[i] == 0) or (np.isnan(target_planes_num[i])):
+                new_count[year][target_plane] = np.nan
+            else:
+                new_count[year][target_plane] = int(target_planes_num[i])
+        #print(year, new_count[year])
+    return new_count
 
-def plotPlanes(count, years, all_planes, name):
-    print('Plotting for {}...'.format(name))
+def plotPlanes(count, years, target_planes, name):
+    print('Plotting for {}'.format(name))
 
     # Make a helper variable for plotting.
-    deliveries_unpacked = {plane: [count[year][plane] if plane in count[year].keys() else np.nan for year in years] for plane in all_planes}
+    deliveries_unpacked = {plane: [count[year][plane] if plane in count[year].keys() else np.nan for year in years] for plane in target_planes}
 
     # Plot of deliveries over time by plane.
     plt.figure(figsize=(8, 6), dpi=300)
     for plane in np.sort(list(deliveries_unpacked.keys())):
         plt.plot(years, deliveries_unpacked[plane], label=plane)
-    plt.xticks(years[::-10])
-    plt.ylabel('Deliveries')
+    plt.ylim([0, 700])
+    plt.xlim([1955, 2030])
+    plt.ylabel('Deliveries per year')
     plt.xlabel('Year')
-    plt.legend()
-    plt.savefig('plots/{}.png'.format(name))
+    plt.title(name)
+    plt.legend(loc='upper left')
+    plt.savefig('plots/{}_{}-{}.png'.format(name, years[0], years[-1]))
 
 count_boeing = Boeing()
 count_airbus = Airbus()
 
-count_all = {}
-for year in np.unique(np.append(list(count_boeing.keys()), list(count_airbus.keys()))):
-    if (year in count_boeing.keys()) and (year in count_airbus.keys()):
-        count_all[int(year)] = { **count_boeing[year], **count_airbus[year] }
-    elif (year in count_boeing.keys()):
-        count_all[int(year)] = count_boeing[year]
-    elif (year in count_airbus.keys()):
-        count_all[int(year)] = count_airbus[year]
-    print(year, count_all[year])
-with open('exports/airplane-manufacturing-preprocessing.json', 'w') as fp:
-    json.dump(count_all , fp)
+def exportData(count_boeing, count_airbus):
+    print('Exporting data')
+    count_all = {}
+    for year in np.unique(np.append(list(count_boeing.keys()), list(count_airbus.keys()))):
+        if (year in count_boeing.keys()) and (year in count_airbus.keys()):
+            count_all[int(year)] = { **count_boeing[year], **count_airbus[year] }
+        elif (year in count_boeing.keys()):
+            count_all[int(year)] = count_boeing[year]
+        elif (year in count_airbus.keys()):
+            count_all[int(year)] = count_airbus[year]
+    with open('exports/preprocessing.json', 'w') as fp:
+        json.dump(count_all , fp)
+
+exportData(count_boeing, count_airbus)
